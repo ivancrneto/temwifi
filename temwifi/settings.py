4622002265ea6f88135462ea14a3f832bdf56506
Ivan Neto <ivan.cr.neto@gmail.com>
2017-08-05 16:49:34 -0300

    added django templates along with jinja

--- temwifi/settings.py
+++ temwifi/settings.py
@@ -77,6 +77,19 @@ TEMPLATES = [
             "undefined": 'jinja2.Undefined',
         },
     },
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = 'temwifi.wsgi.application'
