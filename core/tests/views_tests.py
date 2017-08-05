9fe3442abf8312192319b38f80bfc7dff99eaebe
Ivan Neto <ivan.cr.neto@gmail.com>
2017-08-05 17:55:57 -0300

    restricted places queryset

--- core/tests/views_tests.py
+++ core/tests/views_tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from model_mommy import mommy
 
 from core import views
 
@@ -12,3 +13,13 @@ class PlacesListViewTest(TestCase):
         self.assertIsInstance(self.view, views.ListView)
         self.assertEqual(self.view.model, views.Place)
         self.assertEqual(self.view.context_object_name, 'places')
+        self.assertEqual(self.view.template_name, 'core/place_list.jinja')
+
+    def test_get_queryset(self):
+        place1 = mommy.make('core.Place')
+        place2 = mommy.make('core.Place')
+        mommy.make('core.Rating', place=place1)
+
+        queryset = self.view.get_queryset()
+        self.assertIn(place1, queryset)
+        self.assertNotIn(place2, queryset)
--- core/views.py
+++ core/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
 
 from core.models import Place
@@ -9,3 +8,4 @@ class PlacesListView(ListView):
     model = Place
     context_object_name = 'places'
     template_name = 'core/place_list.jinja'
+    queryset = Place.objects.filter(ratings__isnull=False)
