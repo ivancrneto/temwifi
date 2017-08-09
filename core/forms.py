36318ca210c575404f2bf29c7f50e5af4e5908d1
Ivan Neto <ivan.cr.neto@gmail.com>
2017-08-09 04:22:39 -0300

    added some validations

--- core/forms.py
+++ core/forms.py
@@ -26,3 +26,33 @@ class InternetRatingForm(forms.ModelForm):
     class Meta:
         fields = '__all__'
         model = InternetRating
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        exists = cleaned_data['exists']
+        speed = cleaned_data['speed']
+        is_open = cleaned_data['is_open']
+        password = cleaned_data['password']
+
+        if not exists and (speed or is_open or password):
+            self.add_error(
+                'exists', 'This field is required when you fill the others '
+                'below.')
+
+        if exists and not speed:
+            self.add_error(
+                'speed', 'This field is required you check that there is '
+                'internet.')
+
+        if not is_open and not password:
+            self.add_error(
+                'password', 'This field is required you check that the '
+                'internet is not open.')
+
+        if is_open and password:
+            self.add_error(
+                'is_open', 'This field must be unchecked when you fill a '
+                'password.')
+
+        return cleaned_data
